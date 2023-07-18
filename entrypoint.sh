363fd8ce1a723fe95a378279ea5f42ee82da690c
cfs4819 <1294397380@qq.com>
2023-07-18 09:45:13 +0800

    change entrypoint arg 'filename' into 'port'.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ export PYTHONPATH=$PYTHONPATH:/home/autoware/PythonAPI/$PYTHON2_EGG
 source /home/autoware/carla_ws/devel/setup.bash
 source /home/autoware/Autoware/install/setup.bash
 
-echo arg1 town: $1, arg2 spawn_point: $2
-roslaunch carla_autoware_agent carla_autoware_agent.launch town:=$1 spawn_point:=$2 filename:=$3
+echo arg1 town: $1, arg2 spawn_point: $2, arg3 port: $3
+roslaunch carla_autoware_agent carla_autoware_agent.launch town:=$1 spawn_point:=$2 port:=$3
 tail -f /dev/null
